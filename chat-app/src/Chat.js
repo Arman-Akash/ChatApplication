06c7bf8b7baedbf294ea9b47c9221752bea94d68
arman <arman.akash20@yahoo.com>
2023-04-17 14:59:01 +0600

    design changed

--- chat-app/src/Chat.js
+++ chat-app/src/Chat.js
@@ -14,85 +14,146 @@ const Chat = () => {
     //source: https://bootsnipp.com/snippets/nNg98
 
     return (
-        <div className="container-fluid h-100">
-            <div className="row justify-content-center h-100">
-                <div className="col-md-4 col-xl-3 chat"><div className="card mb-sm-3 mb-md-0 contacts_card">
-                    <div className="card-header">
-                        <div className="input-group">
-                            <input type="text" placeholder="Search..." name="" className="form-control search" />
-                            <div className="input-group-prepend">
-                                <span className="input-group-text search_btn"><i className="fas fa-search"></i></span>
+        <div className="container-fluid vh-100">
+            <div className="row vh-100" >
+                <div className="col-md-4 col-xl-3 chat pl-0 pr-0">
+                    <div className="card mb-sm-3 mb-md-0 contacts_card br-0">
+                        <div className="card-header">
+                            <div className="input-group">
+                                <input type="text" placeholder="Search..." name="" className="form-control search" />
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text search_btn"><i className="fas fa-search"></i></span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="card-body contacts_body">
-                        <ui className="contacts">
-                            <li className="active">
-                                <div className="d-flex bd-highlight">
-                                    <div className="img_cont">
-                                        <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" className="rounded-circle user_img" />
-                                        <span className="online_icon"></span>
+                        <div className="card-body contacts_body">
+                            <ui className="contacts">
+                                <li className="active">
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Khalid</span>
+                                            <p>Kalid is online</p>
+                                        </div>
                                     </div>
-                                    <div className="user_info">
-                                        <span>Khalid</span>
-                                        <p>Kalid is online</p>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon offline"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Taherah Big</span>
+                                            <p>Taherah left 7 mins ago</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="d-flex bd-highlight">
-                                    <div className="img_cont">
-                                        <img src="https://2.bp.blogspot.com/-8ytYF7cfPkQ/WkPe1-rtrcI/AAAAAAAAGqU/FGfTDVgkcIwmOTtjLka51vineFBExJuSACLcBGAs/s320/31.jpg" className="rounded-circle user_img" />
-                                        <span className="online_icon offline"></span>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Sami Rafi</span>
+                                            <p>Sami is online</p>
+                                        </div>
                                     </div>
-                                    <div className="user_info">
-                                        <span>Taherah Big</span>
-                                        <p>Taherah left 7 mins ago</p>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Sami Rafi</span>
+                                            <p>Sami is online</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="d-flex bd-highlight">
-                                    <div className="img_cont">
-                                        <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
-                                        <span className="online_icon"></span>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Sami Rafi</span>
+                                            <p>Sami is online</p>
+                                        </div>
                                     </div>
-                                    <div className="user_info">
-                                        <span>Sami Rafi</span>
-                                        <p>Sami is online</p>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Sami Rafi</span>
+                                            <p>Sami is online</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="d-flex bd-highlight">
-                                    <div className="img_cont">
-                                        <img src="http://profilepicturesdp.com/wp-content/uploads/2018/07/sweet-girl-profile-pictures-9.jpg" className="rounded-circle user_img" />
-                                        <span className="online_icon offline"></span>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Sami Rafi</span>
+                                            <p>Sami is online</p>
+                                        </div>
                                     </div>
-                                    <div className="user_info">
-                                        <span>Nargis Hawa</span>
-                                        <p>Nargis left 30 mins ago</p>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://i.pinimg.com/originals/ac/b9/90/acb990190ca1ddbb9b20db303375bb58.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Sami Rafi</span>
+                                            <p>Sami is online</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="d-flex bd-highlight">
-                                    <div className="img_cont">
-                                        <img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" className="rounded-circle user_img" />
-                                        <span className="online_icon offline"></span>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="http://profilepicturesdp.com/wp-content/uploads/2018/07/sweet-girl-profile-pictures-9.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon offline"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Nargis Hawa</span>
+                                            <p>Nargis left 30 mins ago</p>
+                                        </div>
                                     </div>
-                                    <div className="user_info">
-                                        <span>Rashid Samim</span>
-                                        <p>Rashid left 50 mins ago</p>
+                                </li>
+                                <li>
+                                    <div className="d-flex bd-highlight">
+                                        <div className="img_cont">
+                                            <img src="https://static.turbosquid.com/Preview/001214/650/2V/boy-cartoon-3D-model_D.jpg" className="rounded-circle user_img" />
+                                            <span className="online_icon offline"></span>
+                                        </div>
+                                        <div className="user_info">
+                                            <span>Rashid Samim</span>
+                                            <p>Rashid left 50 mins ago</p>
+                                        </div>
                                     </div>
-                                </div>
-                            </li>
-                        </ui>
-                    </div>
-                    <div className="card-footer"></div>
-                </div></div>
-                <div className="col-md-8 col-xl-6 chat">
-                    <div className="card">
+                                </li>
+                            </ui>
+                        </div>
+                        <div className="card-footer"></div>
+                    </div></div>
+                <div className="col-md-8 col-xl-9 chat pl-0 pr-0">
+                    <div className="card br-0">
                         <div className="card-header msg_head">
                             <div className="d-flex bd-highlight">
                                 <div className="img_cont">
@@ -100,15 +161,15 @@ const Chat = () => {
                                     <span className="online_icon"></span>
                                 </div>
                                 <div className="user_info">
-                                    <span>Chat with Khalid</span>
-                                    <p>1767 Messages</p>
+                                    <span>Khalid</span>
+                                    {/* <p className="mb-0">1767 Messages</p> */}
                                 </div>
-                                <div className="video_cam">
+                                {/* <div className="video_cam">
                                     <span><i className="fas fa-video"></i></span>
                                     <span><i className="fas fa-phone"></i></span>
-                                </div>
+                                </div> */}
                             </div>
-                            <span id="action_menu_btn"><i className="fas fa-ellipsis-v"></i></span>
+                            {/* <span id="action_menu_btn"><i className="fas fa-ellipsis-v"></i></span>
                             <div className="action_menu">
                                 <ul>
                                     <li><i className="fas fa-user-circle"></i> View profile</li>
@@ -116,7 +177,7 @@ const Chat = () => {
                                     <li><i className="fas fa-plus"></i> Add to group</li>
                                     <li><i className="fas fa-ban"></i> Block</li>
                                 </ul>
-                            </div>
+                            </div> */}
                         </div>
                         <div className="card-body msg_card_body">
                             <div className="d-flex justify-content-start mb-4">
@@ -180,6 +241,15 @@ const Chat = () => {
                                 <div className="msg_cotainer">
                                     Bye, see you
                                     <span className="msg_time">9:12 AM, Today</span>
+                                </div>
+                            </div>
+                            <div className="d-flex justify-content-end mb-4">
+                                <div className="msg_cotainer_send">
+                                    Ok
+                                    <span className="msg_time_send">9:10 AM, Today</span>
+                                </div>
+                                <div className="img_cont_msg">
+                                    <img src="https://static.turbosquid.com/Preview/001292/481/WV/_D.jpg" className="rounded-circle user_img_msg" />
                                 </div>
                             </div>
                         </div>
